1e7c44338be7d02c2910fcc7584b97faccdec260
Saidmurod Sabirov <sabir.dev7@gmail.com>
2025-05-14 18:45:52 +0100

    ios safari iframe issue

--- components/EasyTaxiWidget/Booking.vue
+++ components/EasyTaxiWidget/Booking.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="booking-iframe ">
+  <div class="booking-iframe">
     <iframe
       ref="bookingIframe"
-      src="https://u3396.eto2.taxi/booking?site_key=7e3f3d3085b900d598bc40543d611575"
       id="eto-iframe-booking"
+      :src="iframeSrc"
       allow="geolocation"
       width="100%"
       height="250"
@@ -15,24 +15,41 @@
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
+
+// Your EasyTaxiOffice site key and vendor URL
+const siteKey = '7e3f3d3085b900d598bc40543d611575'
+const vendorUrl = `https://u3396.eto2.taxi/booking?site_key=${siteKey}`
+
+// Reactive iframe source defaults to the vendor URL
+const iframeSrc = ref(vendorUrl)
+
+// Detect iOS Safari (excludes Chrome/Firefox on iOS)
+function isIosSafari() {
+  const ua = navigator.userAgent
+  return /iP(hone|od|ad)/.test(ua) && /Safari/.test(ua) && !/CriOS|FxiOS/.test(ua)
+}
+
 onMounted(() => {
-  const script = document.createElement("script");
-  script.src =
-    "https://u3396.eto2.taxi/assets/plugins/iframe-resizer/iframeResizer.min.js";
+  if (isIosSafari()) {
+    // Redirect iOS Safari users directly to the vendor URL
+    window.location.replace(vendorUrl)
+    return
+  }
+
+  // Load the iframe-resizer script for all other browsers
+  const script = document.createElement('script')
+  script.src = 'https://u3396.eto2.taxi/assets/plugins/iframe-resizer/iframeResizer.min.js'
   script.onload = () => {
     if (window.iFrameResize) {
       window.iFrameResize(
-        {
-          log: false,
-          targetOrigin: "*",
-          checkOrigin: false,
-        },
-        "#eto-iframe-booking"
-      );
+        { log: false, targetOrigin: '*', checkOrigin: false },
+        '#eto-iframe-booking'
+      )
     }
-  };
-  document.head.appendChild(script);
-});
+  }
+  document.head.appendChild(script)
+})
 </script>
 
 <style scoped>
--- components/EasyTaxiWidget/MyAccount.vue
+++ components/EasyTaxiWidget/MyAccount.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="customer-iframe">
+  <div class="booking-widget">
     <iframe
-      ref="customerIframe"
-      src="https://u3396.eto2.taxi/customer?site_key=7e3f3d3085b900d598bc40543d611575"
-      id="eto-iframe-customer"
+      ref="bookingIframe"
+      id="eto-iframe-booking-widget"
+      :src="iframeSrc"
       allow="geolocation"
       width="100%"
       height="250"
@@ -15,28 +15,44 @@
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
+
+// Define your site key and vendor URL\ n
+const siteKey = '7e3f3d3085b900d598bc40543d611575'
+const vendorUrl = `https://u3396.eto2.taxi/booking/widget?site_key=${siteKey}`
+
+// Reactive iframe source defaults to vendor URL\ nconst iframeSrc = ref(vendorUrl)
+
+// Detect iOS Safari
+function isIosSafari() {
+  const ua = navigator.userAgent
+  return /iP(hone|od|ad)/.test(ua) && /Safari/.test(ua) && !/CriOS|FxiOS/.test(ua)
+}
+
 onMounted(() => {
-  const script = document.createElement("script");
-  script.src =
-    "https://u3396.eto2.taxi/assets/plugins/iframe-resizer/iframeResizer.min.js";
+  if (isIosSafari()) {
+    // Redirect iOS Safari users to the vendor URL
+    window.location.replace(vendorUrl)
+    return
+  }
+
+  // Load iframe-resizer script for all non-iOS Safari users
+  const script = document.createElement('script')
+  script.src = 'https://u3396.eto2.taxi/assets/plugins/iframe-resizer/iframeResizer.min.js'
   script.onload = () => {
     if (window.iFrameResize) {
       window.iFrameResize(
-        {
-          log: false,
-          targetOrigin: "*",
-          checkOrigin: false,
-        },
-        "#eto-iframe-customer"
-      );
+        { log: false, targetOrigin: '*', checkOrigin: false },
+        '#eto-iframe-booking-widget'
+      )
     }
-  };
-  document.head.appendChild(script);
-});
+  }
+  document.head.appendChild(script)
+})
 </script>
 
 <style scoped>
-.customer-iframe {
+.booking-widget {
   width: 100%;
 }
 </style>
