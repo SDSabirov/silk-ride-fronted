6deb9402baed8d0cb57f57ad5e92ba7e085da8da
Saidmurod Sabirov <sabir.dev7@gmail.com>
2025-05-28 14:49:42 +0100

    yandex metrica

--- plugins/yandex-metrica.client.ts
+++ plugins/yandex-metrica.client.ts
@@ -1,27 +1,30 @@
 export default defineNuxtPlugin(() => {
   if (process.client) {
+    // Skip if already loaded
+    if (typeof window.ym === 'function') return;
+
     (function(m, e, t, r, i, k, a) {
       m[i] = m[i] || function() {
         (m[i].a = m[i].a || []).push(arguments)
       };
       m[i].l = 1 * new Date();
       for (var j = 0; j < document.scripts.length; j++) {
-        if (document.scripts[j].src === r) {
-          return;
-        }
+        if (document.scripts[j].src === r) return;
       }
       k = e.createElement(t), a = e.getElementsByTagName(t)[0];
       k.async = 1;
       k.src = r;
+      k.onload = () => {
+        if (typeof window.ym === 'function') {
+          window.ym(102229971, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true,
+          });
+        }
+      };
       a.parentNode.insertBefore(k, a);
     })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    // Initialize Yandex Metrika
-    
-    window.ym?.(102229971, "init", {
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true
-    });
   }
 });
