ca2f4d7ef4bf0e850fdfda2a94c56f2be0b9cee4
Saidmurod Sabirov <sabir.dev7@gmail.com>
2025-05-29 14:36:02 +0100

    metrica

--- plugins/yandex-metrica.client.ts
+++ plugins/yandex-metrica.client.ts
@@ -1,16 +1,13 @@
 export default defineNuxtPlugin(() => {
   if (process.client) {
-    // Skip if already loaded
-    if (typeof window.ym === 'function') return;
+    // Skip if script already added
+    if (document.querySelector('script[src="https://mc.yandex.ru/metrika/tag.js"]')) return;
 
-    (function(m, e, t, r, i, k, a) {
-      m[i] = m[i] || function() {
+    (function (m, e, t, r, i, k, a) {
+      m[i] = m[i] || function () {
         (m[i].a = m[i].a || []).push(arguments)
       };
       m[i].l = 1 * new Date();
-      for (var j = 0; j < document.scripts.length; j++) {
-        if (document.scripts[j].src === r) return;
-      }
       k = e.createElement(t), a = e.getElementsByTagName(t)[0];
       k.async = 1;
       k.src = r;
